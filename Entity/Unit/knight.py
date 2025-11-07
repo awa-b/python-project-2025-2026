1b07e1196d8d088edf5fb7836fbc6455e4149716
Awa_Bathily <bathilyawa346@gmail.com>
2025-11-07 15:51:14 +0100

    Ajout commentaires

--- Entity/Unit/knight.py
+++ Entity/Unit/knight.py
@@ -18,27 +18,19 @@ class Knight(Guerrier):
 
     def se_deplacer(self):
         print("Knight moves")
-        
 
-<<<<<<< HEAD
-    def attaquer(self):
-        print("The knight attacks")
-
-    def calculer_degats(self, cible):
-        """calcul des dégats pour Knight"""
-=======
     def attaquer(self, target, currentTime):
         if not self.isAlive or not target.isAlive:
-            return False
+            return False #Impossible fight if one of the two is dead 
         
         if currentTime - self.lastAttackTime < self.reloadTime:
-            return False  
+            return False #The reload time must first have elapsed  
         
         distance_to_target = self._calculate_distance(target)
-
-        if distance_to_target > 1.0:
-            return False
-        else:
+ 
+        if distance_to_target > self.range:
+            return False #The target is out of range
+        else: #Attack
             damage = self._calculate_damage(target)
             target.hp -= damage
             self.lastAttackTime = currentTime
@@ -48,18 +40,17 @@ class Knight(Guerrier):
     
 
 
-    def _calculate_distance(self, target):
+    def _calculate_distance(self, target): 
         dx = self.position[0] - target.position[0]
         dy = self.position[1] - target.position[1]
         return math.sqrt(dx**2 + dy**2)
     
 
-    def _calculate_damage(self, target):
-        k_elev = 1.0
+    def _calculate_damage(self, target): 
+        k_elev = 1.0 #No elevation for the moment
         damage = self.baseMelee
         damage -= target.armor
         damage = k_elev * damage
-        damage = max(1, damage)
+        damage = max(1, damage) #It must have at least one damage
         return damage
     
->>>>>>> methods-knight
