30b7c6cf9cd21f6f471dc5d299c7ad5dbb38c4e2
MineDX10 <119359999+MineDX10@users.noreply.github.com>
2025-11-07 13:06:24 +0100

    knight.py

--- Entity/Unit/knight.py
+++ Entity/Unit/knight.py
@@ -5,7 +5,7 @@ class Knight(Guerrier):
         super().__init__(hp=100, attaque=10, armor=2, pierceArmor=2, range=0, lineOfSight=4, speed=1.35, buildTime=30,
                          reloadTime=1.8, standardBuildings=0, cavalryResistance=-3, baseMelee=10, allBuildings=0,
                          allArchers=0, skirmishers=0, siegeUnits=0, obsolete=0, mountedUnits=0, baseMeleeDefense=2,
-                         basePierce=2, obsoleteDefense=0)
+                         basePierce=2, obsoleteDefense=0, cooldown=0)
         
 
     def se_deplacer(self):
