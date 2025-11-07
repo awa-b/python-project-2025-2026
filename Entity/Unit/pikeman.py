1b563e0747f249a59360ef12ce9e27db3767830b
MineDX10 <119359999+MineDX10@users.noreply.github.com>
2025-11-07 13:06:43 +0100

    Update pikeman.py

--- Entity/Unit/pikeman.py
+++ Entity/Unit/pikeman.py
@@ -5,7 +5,7 @@ class Pikeman(Guerrier):
         super().__init__(hp=55, attaque=4, armor=0, pierceArmor=0, range=0, lineOfSight=4, speed=1, buildTime=22, reloadTime=3,
                     shockInfantry=1, standardBuildings=1, elephants=25, baseMelee=4, mountedUnits=22, ships=16, camels=18,
                     mamelukes=7, fishingShips=16, allArchers=0, 
-                    spearUnits=0, infantry=0, baseMeleeDefense=0, basePierce=0, obsoleteDefense=0)
+                    spearUnits=0, infantry=0, baseMeleeDefense=0, basePierce=0, obsoleteDefense=0, cooldown=0)
         
 
     def se_deplacer(self):
