c715abacfd44bb7c734f8a3855674fc75a330acb
MineDX10 <119359999+MineDX10@users.noreply.github.com>
2025-12-01 14:26:07 +0100

    majorDaft.py

--- Models/IA Generales/majorDaft.py
+++ Models/IA Generales/majorDaft.py
@@ -15,11 +15,15 @@ class MajorDaft(General):
                     troupe[1].cooldown+=1/troupe[1].speed #add delay to the troop before its next action
                     print(f"{troupe} se déplace")
             else:    
-                Map[cible1[0]][cible1[1]][cible1[2]][1].hp-=troupe[1].attaque #if there was an enemy in its attack range, attack the enemy
+                ennemi = Map.to_matrix[cible1[0]][cible1[1]][cible1[2]][1]
+                team_ennemi = Map.to_matrix[cible1[0]][cible1[1]][cible1[2]][0]
+                ennemi.hp-=troupe[1].attaque(troupe[1],ennemi) #if there was an enemy in its attack range, attack the enemy
                 troupe[1].cooldown+=troupe[1].reloadTime + troupe[1].attackDelay #add delay to the troop before its next action
-                print(f"{troupe} attaque {cible1} ")
+                print(f"{troupe} attaque {ennemi} en {cible1[0]} ,  {cible1[1]}")
                 if Map[cible1[0]][cible1[1]][cible1[2]][1].hp <=0:
-                    Map=Map[cible1[0]][cible1[1]][:cible1[2]] + Map[cible1[0]][cible1[1]][cible1[2]+1:] # if the enemy is dead, remove it from the map 
+                    Map.remove_unit(Map, cible1[0] , cible1[1] , team_ennemi, ennemi)
+                print(f"{ennemi} {team_ennemi} est mort") 
         else:
             troupe[1].cooldown-= 1 # if the troop did nothing, reduce its delay
         return Map
+        
