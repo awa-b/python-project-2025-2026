938e21ff7284fabaad947bb63a04069b7421e216
MineDX10 <119359999+MineDX10@users.noreply.github.com>
2025-12-01 14:08:42 +0100

    Map.py
    ajout de remove_unit

--- Models/Map/Map.py
+++ Models/Map/Map.py
@@ -26,6 +26,18 @@ class BattleMap:
             raise ValueError(f"Case ({row}, {col}) hors de la map")
         self.grid[row][col].append([team, unit])
 
+    def remove_unit(self, row: int, col: int, team: str, unit) -> None:
+        """
+        Supprime une unité morte de la case (row, col)
+        team = 'r' (rouge), 'b' (bleu), etc.
+        unit = objet Knight / Pikeman / Crossbowman...
+        """
+        for i in len(self.grid[row][col]): ## trouve la position de l'unité morte dans la case (row,col)
+            if self.grid[row][col][i] == [team,unit]:
+                indice=i
+        self.grid[row][col] = self.grid[row][col][:indice] + self.grid[row][col][indice+1:] ## supprime l'unité
+   
+
     def get_units(self, row: int, col: int):
         if not self.in_bounds(row, col):
             return []
